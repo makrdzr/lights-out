8a52411397f10bf4eb7eeb481f6a88f17fded338
Makar Dzhehur <vt231_dmyu@student.ztu.edu.ua>
2026-04-06 10:53:55 +0300

    fix: add basename to router for correct navigation on GitHub Pages

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,24 +4,29 @@ import StartPage from "../pages/StartPage";
 import GamePage from "../pages/GamePage";
 import NotFoundPage from "../pages/NotFoundPage";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <NotFoundPage />,
+      children: [
+        {
+          index: true,
+          element: <StartPage />,
+        },
+        {
+          path: "game/:userId",
+          element: <GamePage />,
+        },
+        {
+          path: "*",
+          element: <NotFoundPage />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        index: true,
-        element: <StartPage />,
-      },
-      {
-        path: "game/:userId",
-        element: <GamePage />,
-      },
-      {
-        path: "*",
-        element: <NotFoundPage />,
-      },
-    ],
+    basename: import.meta.env.BASE_URL,
   },
-]);
+);
